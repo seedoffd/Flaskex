60759382218e39b44b613fada51ec90f335d1649
seedoffd <seedoffd@gmail.com>
2019-03-20 20:29:22 -0500

    update

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -5,15 +5,34 @@ node {
         sh "ssh    ec2-user@${IP}         sudo yum install git python-pip  -y"
     }
     stage("Clone a repo"){
-        git 'https://github.com/farrukh90/flask-examples.git'
-    }
-    stage("Copy files"){
-        sh "scp -r *   ec2-user@${IP}:/tmp/"
-    }
-    stage("Install requirements"){
-        sh "ssh    ec2-user@${IP}      sudo pip install -r /tmp/requirements.txt"
+        git 'https://github.com/farrukh90/Flaskex.git'
     }
     stage("Run App"){
-        sh "ssh    ec2-user@${IP}  python /tmp/01-hello-world/hello.py"
+        try{
+            sh "ssh    ec2-user@${IP}       sudo mkdir  /flaskex 2> /dev/null"
+        }
+        catch(exc){
+            sh "echo folder exists"
+        }
+    }
+    stage("Copy files"){
+        sh "scp -r *   ec2-user@${IP}:/home/ec2-user/"
+    }
+    stage("Move files to /flaskex"){
+        try{
+            sh "ssh    ec2-user@${IP}      sudo mv  /home/ec2-user/*  /flaskex/"
+        }
+        catch(exc){
+            sh "echo Folders moved"
+        }
+    }
+    stage("Install requirements"){
+        sh "ssh    ec2-user@${IP}      sudo pip install -r /flaskex/requirements.txt"
+    }
+    stage("move service to /etc"){
+        sh "ssh    ec2-user@${IP}      sudo mv /flaskex/flaskex.service /etc/systemd/system"
+    }
+    stage("Start service"){
+        sh "ssh    ec2-user@${IP}       sudo systemctl start flaskex"
     }
 }
